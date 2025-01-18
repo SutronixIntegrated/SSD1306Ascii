b36a8dd46f4e8032172653ddd2803a5e65957ee1
Rakesh <rakesh@sutronix.com>
2025-01-18 11:41:21 +0000

    Refactor SSD1306 initialization header to include command definitions from SSD1306Cmd.h

--- src/SSD1306init.h
+++ src/SSD1306init.h
@@ -38,64 +38,7 @@
 #define MEM_TYPE __attribute__((progmem))
 #endif  // __AVR__
 //------------------------------------------------------------------------------
-/** Set Lower Column Start Address for Page Addressing Mode. */
-#define SSD1306_SETLOWCOLUMN 0x00
-/** Set Higher Column Start Address for Page Addressing Mode. */
-#define SSD1306_SETHIGHCOLUMN 0x10
-/** Set Memory Addressing Mode. */
-#define SSD1306_MEMORYMODE 0x20
-/** Set display RAM display start line register from 0 - 63. */
-#define SSD1306_SETSTARTLINE 0x40
-/** Set Display Contrast to one of 256 steps. */
-#define SSD1306_SETCONTRAST 0x81
-/** Enable or disable charge pump.  Follow with 0X14 enable, 0X10 disable. */
-#define SSD1306_CHARGEPUMP 0x8D
-/** Set Segment Re-map between data column and the segment driver. */
-#define SSD1306_SEGREMAP 0xA0
-/** Resume display from GRAM content. */
-#define SSD1306_DISPLAYALLON_RESUME 0xA4
-/** Force display on regardless of GRAM content. */
-#define SSD1306_DISPLAYALLON 0xA5
-/** Set Normal Display. */
-#define SSD1306_NORMALDISPLAY 0xA6
-/** Set Inverse Display. */
-#define SSD1306_INVERTDISPLAY 0xA7
-/** Set Multiplex Ratio from 16 to 63. */
-#define SSD1306_SETMULTIPLEX 0xA8
-/** Set Display off. */
-#define SSD1306_DISPLAYOFF 0xAE
-/** Set Display on. */
-#define SSD1306_DISPLAYON 0xAF
-/**Set GDDRAM Page Start Address. */
-#define SSD1306_SETSTARTPAGE 0XB0
-/** Set COM output scan direction normal. */
-#define SSD1306_COMSCANINC 0xC0
-/** Set COM output scan direction reversed. */
-#define SSD1306_COMSCANDEC 0xC8
-/** Set Display Offset. */
-#define SSD1306_SETDISPLAYOFFSET 0xD3
-/** Sets COM signals pin configuration to match the OLED panel layout. */
-#define SSD1306_SETCOMPINS 0xDA
-/** This command adjusts the VCOMH regulator output. */
-#define SSD1306_SETVCOMDETECT 0xDB
-/** Set Display Clock Divide Ratio/ Oscillator Frequency. */
-#define SSD1306_SETDISPLAYCLOCKDIV 0xD5
-/** Set Pre-charge Period */
-#define SSD1306_SETPRECHARGE 0xD9
-/** Deactivate scroll */
-#define SSD1306_DEACTIVATE_SCROLL 0x2E
-/** No Operation Command. */
-#define SSD1306_NOP 0XE3
-//------------------------------------------------------------------------------
-/** Set Pump voltage value: (30H~33H) 6.4, 7.4, 8.0 (POR), 9.0. */
-#define SH1106_SET_PUMP_VOLTAGE 0X30
-/** First byte of set charge pump mode */
-#define SH1106_SET_PUMP_MODE 0XAD
-/** Second byte charge pump on. */
-#define SH1106_PUMP_ON 0X8B
-/** Second byte charge pump off. */
-#define SH1106_PUMP_OFF 0X8A
-//------------------------------------------------------------------------------
+#include "SSD1306Cmd.h"
 /**
  * @struct DevType
  * @brief Device initialization structure.
